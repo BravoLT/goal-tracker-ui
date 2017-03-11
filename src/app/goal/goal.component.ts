0ca2fc943af2c10fa259305f744a6abd434ba936
Michael Nishizawa <michael@fortitude-solutions.com>
2017-03-11 18:00:04 -0500

    pre rebase

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -12,11 +12,30 @@ export class GoalComponent implements OnInit {
   ngOnInit() {
   }
 
+/*
+This is a fairly naive implementation, should probably use the graph db to walk the 
+tree and aggregate percentages.
+*/
+  percentComplete() {
+    if (this.subGoals.length == 0) {
+      return this.done ? 1 : 0
+    }
+
+    let completedGoals: number = 0
+    for(let subgoal of this.subGoals) {
+      if(subgoal.done) {
+        completedGoals++
+      }
+    }
+
+    return completedGoals/this.subGoals.length
+  }
+
   id: string
   description: string
   subGoals: GoalComponent[]
   startDate: string
   endDate: string
   who: string
-
+  done: boolean
 }
