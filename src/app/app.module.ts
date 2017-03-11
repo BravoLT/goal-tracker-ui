4112328213e15c397e25877b216e468afd4b7b1e
Michael Nishizawa <michael@fortitude-solutions.com>
2017-03-11 16:52:10 -0500

    added component and basic properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent
   ],
   imports: [
     BrowserModule,
