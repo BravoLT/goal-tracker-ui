b96f844f6431e45319f9f990a8c93183630ab662
Michael Nishizawa <michael@fortitude-solutions.com>
2017-03-11 11:08:59 -0500

    initial project

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { GoalTrackerUiPage } from './app.po';
+
+describe('goal-tracker-ui App', function() {
+  let page: GoalTrackerUiPage;
+
+  beforeEach(() => {
+    page = new GoalTrackerUiPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
