baa5c088d7c96e0f611267e97efeba19dfa20a4c
Nick Butcher <corsairmarks@hotmail.com>
2017-03-12 05:31:52 -0400

    #4 fix service endpoint - goals whether get all or singular

--- src/app/goal.service.ts
+++ src/app/goal.service.ts
@@ -5,19 +5,19 @@ import { Goal } from './goal';
 
 @Injectable()
 export class GoalService {
-  private goalServiceUrl : string = 'http://localhost:8080/';
+  private goalServiceUrl : string = 'http://localhost:8080/goals';
 
   constructor(private http: Http) {
   }
 
   getGoals() : Observable<Goal[]> {
-    return this.http.get(`${this.goalServiceUrl}goals`)
+    return this.http.get(this.goalServiceUrl)
               .map(res => res.json())
               .catch(error => Observable.throw(error.json().error || 'Server error'));
   }
 
   getGoal(id : string) : Observable<Goal> {
-    return this.http.get(`${this.goalServiceUrl}goal/${id}`)
+    return this.http.get(`${this.goalServiceUrl}/${id}`)
               .map(res => res.json())
               .catch(error => Observable.throw(error.json().error || 'Server error'));
   }
