e44ab4e1f43e93e331c8fc23b2bd8ac6cb739547
Michael Nishizawa <michael@fortitude-solutions.com>
2017-03-11 23:22:26 -0500

    goal route works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GoalComponent } from './goal/goal.component'
 
 const routes: Routes = [
   {
     path: '',
+    children: []
+  },
+  {
+    path: 'goal',
+    component: GoalComponent,
     children: []
   }
 ];
